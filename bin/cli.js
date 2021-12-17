160ee9aa4b8b29c9a4de54ab928c21540c59a207
Sergio de Cristofaro <sdecri@gmail.com>
2021-12-17 14:32:40 +0100

    refactor: changed name to tap-nirvana-extended

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
-const tapBDiff = require('../tap-nirvana')();
+const tapBDiff = require("../tap-nirvana-extended")();
 
-process.stdin
-  .pipe(tapBDiff)
-  .pipe(process.stdout);
+process.stdin.pipe(tapBDiff).pipe(process.stdout);
 
-process.on('exit', function (status) {
-  
+process.on("exit", function (status) {
   if (status === 1) {
     process.exit(1);
   }
-  
+
   if (tapBDiff.failed) {
     process.exit(1);
   }
